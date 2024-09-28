bb927c3b9115361240a51308d011533cff95c45b
lyannne <xu.ly@northeastern.edu>
2024-09-28 15:55:48 -0400

    CRUD and user entity

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'bagly',
+      entities: [],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/app/users.entity.ts
@@ -0,0 +1,42 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail} from 'class-validator';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({length: 50})
+    name: string;
+
+    @Column()
+    @IsEmail()
+    email_address: string;
+
+    // role: unfinished
+    @Column()
+
+
+    @Column()
+    verified: boolean;
+
+    @Column()
+    private age: number;
+
+    set set_age(value: number){
+        if (value < 0){
+            value = 0;
+        }
+        else {
+            this.age = value;
+        }
+    }
+
+    get get_age(): number{
+        return this.age;
+    }
+
+    @Column({type: 'time'})
+    joined_at: string;
+
+}
