bb68790cfcfea4dfdd4fb386df0e46135f15723e
neetidesai <97416423+neetidesai@users.noreply.github.com>
2024-12-19 15:14:21 -0500

    Syntax fixes

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,36 +3,16 @@ import { SafeAreaView, StyleSheet, View } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import Header from '../components/Header';
 import Logo from '../components/Logo';
-import Map from '../components/Map';
 import FiltersDropdown from '../components/FiltersDropdown';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import BottomSheet, { BottomSheetFlatList } from '@gorhom/bottom-sheet';
-import ShelterInfoPanel from '../components/ShelterInfoPanel';
-import { Shelter, shelters } from '../sheltersTest';
 import SignUpModal from '../components/SignUpModal';
 import CompleteMap from '../components/CompleteMap';
-import SignUpModal from '../components/SignUpModal';
 
 export const App = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
-        <CompleteMap />
+      <CompleteMap />
       <View style={styles.centeredView}>
         <SignUpModal />
-      <View style={styles.logoContainer}>
-        <Logo />
-      </View>
-      <View style={styles.centeredView}>
-        <SignUpModal />
-      </View>
-      <View style={styles.searchBarContainer}>
-        <SearchBar />
-      </View>
-      <View style={styles.headerContainer}>
-        <Header />
-      </View>
-      <View style={styles.filtersDropdownContainer}>
-        <FiltersDropdown />
       </View>
     </SafeAreaView>
   );
@@ -48,10 +28,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  logoContainer: {
+    marginBottom: 20,
+  },
+  searchBarContainer: {
+    marginVertical: 10,
+  },
+  headerContainer: {
+    marginTop: 20,
+  },
+  filtersDropdownContainer: {
+    marginVertical: 10,
   },
 });
 
