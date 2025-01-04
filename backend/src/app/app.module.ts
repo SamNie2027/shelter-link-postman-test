f70f45c2cfd50d1f37e7758e63b5c272fd3c46c9
neetidesai <97416423+neetidesai@users.noreply.github.com>
2025-01-04 18:45:18 -0500

    Set up dynamodb

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -2,22 +2,35 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DynamoDbService } from '../dynamodb';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'bagly',
-      entities: [],
-      synchronize: process.env.NODE_ENV !== 'production',
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DynamoDbService],
 })
 export class AppModule {}
+
+// import { Module } from '@nestjs/common';
+
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'mysql',
+//       host: 'localhost',
+//       port: 3306,
+//       username: 'root',
+//       password: 'password',
+//       database: 'bagly',
+//       entities: [],
+//       synchronize: process.env.NODE_ENV !== 'production',
+//     }),
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
---
+++ backend/src/dynamodb.ts
@@ -0,0 +1,17 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DynamoDbService {
+  private readonly dynamoDbClient: DynamoDBClient;
+
+  constructor() {
+    this.dynamoDbClient = new DynamoDBClient({
+      region: process.env.AWS_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+  }
+}
