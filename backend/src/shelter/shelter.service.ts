f0d085ba39bdbd2c97c104e09c2db0ddcb16fb11
neetidesai <97416423+neetidesai@users.noreply.github.com>
2025-01-10 15:24:52 -0500

    Remove old comments

--- backend/src/shelter/shelter.service.ts
+++ backend/src/shelter/shelter.service.ts
@@ -25,8 +25,6 @@ export class ShelterService {
     } catch (e) {
       throw new Error('Unable to post new shelter: ' + e);
     }
-
-    // return this.dynamoDbService.putItem(this.tableName, shelterData);
   }
 
   private postInputToShelterModel = (
@@ -99,68 +97,5 @@ export class ShelterService {
         },
       },
     };
-
-    //   constructor(
-    //     @InjectRepository(Shelter)
-    //     private sheltersRepository: Repository<Shelter>,
-    //   ) {}
-
-    //   // get all shelters
-    //   findAll(): Promise<Shelter[]> {
-    //     return this.sheltersRepository.find();
-    //   }
-
-    //   // get shelters, order by distance
-    //   async findByProximity(lat: number, lon: number, limit = 10): Promise<Shelter[]> {
-    //     // Haversine formula for distance
-    //     const haversine = `
-    //       6371 * acos(
-    //         cos(radians(:lat)) * cos(radians(latitude))
-    //         * cos(radians(longitude) - radians(:lon))
-    //         + sin(radians(:lat)) * sin(radians(latitude))
-    //       )
-    //     `;
-
-    //     const shelters = await this.sheltersRepository
-    //       .createQueryBuilder('shelter')
-    //       .select('shelters.*')
-    //       .addSelect(`${haversine}`, 'distance')
-    //       .setParameter('lat', lat)
-    //       .setParameter('lon', lon)
-    //       .orderBy('distance', 'ASC')
-    //       .limit(limit)
-    //       .getRawMany();
-
-    //     return shelters.map(shelter => {
-    //       const { distance, ...shelterData } = shelter;
-    //       return {
-    //         ...shelterData,
-    //         distance: parseFloat(distance).toFixed(2)
-    //       };
-    //     });
-    //   }
-
-    //   // create
-    //   async create(shelterData: Partial<Shelter>): Promise<Shelter> {
-    //     const shelter = this.sheltersRepository.create(shelterData);
-    //     return await this.sheltersRepository.save(shelter);
-    //   }
-
-    //   // read
-    //   findOne(id: number): Promise<Shelter | null> {
-    //     return this.sheltersRepository.findOneBy({ id });
-    //   }
-
-    //   // update
-    //   async update(id: number, shelterData: Partial<Shelter>): Promise<Shelter> {
-    //     await this.sheltersRepository.update(id, shelterData);
-    //     return this.findOne(id);
-    //   }
-
-    //   // delete
-    //   async remove(id: number): Promise<void> {
-    //     await this.sheltersRepository.delete(id);
-    //   }
-    // }
   };
 }
