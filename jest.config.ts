58be2839bce9cbf435ada851ee9b1053b9d7f5a0
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2025-03-09 21:10:38 -0400

    Added complete tests for shelter.controller.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,6 @@
 import { getJestProjectsAsync } from '@nx/jest';
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
 
 export default async () => ({
   projects: await getJestProjectsAsync(),
